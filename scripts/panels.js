d2d82ebf3092fc361a325e6d60ca4e1a0143682a
Gabewest1 <gabe_west1@yahoo.com>
2017-08-21 16:40:36 -0500

    Created and set handler for when the panel finishes transitioning open and closed.

--- scripts/panels.js
+++ scripts/panels.js
@@ -1,13 +1,63 @@
 import $ from "jquery"
 
-let panels = $(".panel")
-let EXPAND_AMOUNT = `${100/3}%`
-let NORMAL_AMOUNT = `${100/6}%`
-console.log("AYYYY")
-setPanelBackgroundColors()
-handleClick()
+let $panels = $(".panel")
+const EXPAND_AMOUNT = 2.5
+const NORMAL_AMOUNT = 1
 
-function setPanelBackgroundColors() {
+setPanelsBackground()
+setClickHandler()
+setTransitionEnd()
+
+function setTransitionEnd() {
+    $panels.on("transitionend", handleTransitionEnd)
+}
+
+function handleTransitionEnd(e) {
+    let animationName = e.propertyName || e.originalEvent.propertyName
+    
+    console.log("Animation Name:", animationName)
+    if(animationName && animationName.includes("flex")) {
+        transitionPanelText(e.currentTarget)
+    }
+}
+
+function transitionPanelText(panel) {
+    console.log("Transition text for panel:", panel)
+    let $panel = $(panel)
+
+    //Conditional Context: 
+    //This occurs after a panel finishes its flex-grow transition so
+    //we act based off the panels finished state
+    if($panel.css("flexGrow") == NORMAL_AMOUNT) {
+        transitionOutPanelText($panel)
+    } else {
+        transitionInPanelText($panel)
+    }
+}
+
+function transitionInPanelText(panel) {
+    let $panel = $(panel)
+    let $topMessage = $panel.find(".top-message")
+    let $middleMessage = $panel.find(".middle-message")
+    let $bottomMessage = $panel.find(".bottom-message")
+
+    $middleMessage.css("fontSize", "48px")
+    $topMessage.animate({"top": "0"})
+    $bottomMessage.animate({"top": "0"})
+}
+
+function transitionOutPanelText(panel) {
+    let $panel = $(panel)
+    let $topMessage = $panel.find(".top-message")
+    let $middleMessage = $panel.find(".middle-message")
+    let $bottomMessage = $panel.find(".bottom-message")
+    
+    $middleMessage.css("fontSize", "32px")
+    $topMessage.animate({"top": "-100%"})
+    $bottomMessage.animate({"top": "100%"})
+}
+
+function setPanelsBackground() {
     let images = ["panel1.jpg", "panel2.jpg", "panel3.jpg", "panel4.jpg", "panel5.jpg", "panel6.jpg"]
     console.log("AAYYYAYAYAYA")
     panels.each((i, panel) => {
